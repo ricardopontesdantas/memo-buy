cd9145d5ed270cf2eb3fbd6830f263ea116c047e
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-06-09 07:53:24 -0300

    feature(main): throw an error with status code 422 if description is empty

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import express, { Request, Response } from "express";
 const app = express();
 app.use(express.json())
 app.post("/memories", async function(request: Request, response: Response) {
-    if (request.body.description === "") {
-        return response.json({
-            message: "Invalid description"
-        })
-    } else {
+    try {
+        if (request.body.description === "") throw new Error("Invalid description");
         return response.status(201).end();
+    } catch(e: any) {
+        return response.status(422).json({
+            message: e.message
+        });
     }
 });
 app.listen(3000);
