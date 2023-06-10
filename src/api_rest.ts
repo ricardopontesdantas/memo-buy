5d1a6f7c57d49bb381d9639cc818527c92b4cfff
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-06-10 08:44:46 -0300

    feat(api_rest): push a new memory to memories when create one

--- src/api_rest.ts
+++ src/api_rest.ts
@@ -2,9 +2,16 @@ import express, { Request, Response } from "express";
 const app = express();
 app.use(express.json())
 const memories: Memory[] = [];
+
 app.post("/memories", async function(request: Request, response: Response) {
     try {
         if (request.body.description === "") throw new Error("Invalid description");
+        const memory = {
+            memoryId: request.body.memoryId,
+            description: request.body.description,
+            done: false
+        };
+        memories.push(memory);
         return response.status(201).end();
     } catch(e: any) {
         return response.status(422).json({
