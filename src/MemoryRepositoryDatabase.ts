4ad2f16d6e3686381a7c3c8c27de79d1a8a8c66d
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-06-13 08:31:13 -0300

    feat(memory-repository-database): implement methods save and update done

--- src/MemoryRepositoryDatabase.ts
+++ src/MemoryRepositoryDatabase.ts
@@ -1,10 +1,16 @@
+import pgp from "pg-promise";
 import { MemoryRepository } from "./MemoryRespository";
 
 export default class MemoryRepositoryInMemory implements MemoryRepository {
-    save(memory: any): Promise<any> {
-        throw new Error("Method not implemented.");
+    async save(memory: any): Promise<void> {
+        const connection = pgp()("");
+        await connection.query("insert into memory (idMemory, description) values ($1, $2)", [memory.idMemory, memory.description]);
+        connection.$pool.end();
     }
-    updateDone(memoryId: string): Promise<any> {
-        throw new Error("Method not implemented.");
+
+    async updateDone(idMemory: string, done: boolean): Promise<any> {
+        const connection = pgp()("");
+        await connection.query("update memory set done = $1 where id_memory = $2", [done, idMemory]);
+        connection.$pool.end();
     }
 }
