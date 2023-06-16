70f0fc5ce95157e5a7368aa74a60cdc0c135a7a1
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-06-16 13:19:02 -0300

    test(api-rest): create a test to list undone memories from an user

--- test/api_rest.test.ts
+++ test/api_rest.test.ts
@@ -45,3 +45,19 @@ test("Should update status done with true if memory exists", async function() {
     expect(output.done).toBe(true);
     expect(output.idMemory).toBe(idMemory);
 });
+
+test("Should list undone memories from an user", async function() {
+    const idMemory = crypto.randomUUID();
+    const idUser = "fe103b13-e357-4c6c-9aaf-dbe671f18887";
+    const memory = {
+        idMemory,
+        idUser,
+        description: "Buy something",
+        done: false,
+        date: new Date("2023-12-01T10:00:00")
+    };
+    await axios.post("http://localhost:3000/memories", memory);
+    const response = await axios.get(`http://localhost:3000/memories/user/${idUser}`);
+    const output = response.data;
+    expect(output.length > 0).toBe(true);
+});
