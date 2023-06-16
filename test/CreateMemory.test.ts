06de6272cfd0966cd35a9a450193c8abcccb5c80
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-06-16 08:13:55 -0300

    test(create-memory): using test pattern spy and before each

--- test/CreateMemory.test.ts
+++ test/CreateMemory.test.ts
@@ -1,20 +1,35 @@
+import sinon from "sinon";
 import CreateMemory from "../src/CreateMemory";
+import MemoryRepositoryDatabase from "../src/MemoryRepositoryDatabase";
+import crypto from "crypto";
 
-test("Should create a new memory", async function() {
+let createMemory: CreateMemory;
+
+beforeEach(() => {
+    createMemory = new CreateMemory();
+});
+
+test("Should create a new memory using a spy", async function() {
+    const memoryRepositorySpy = sinon.spy(MemoryRepositoryDatabase.prototype, "save");
     const input = {
-        idMemory: "ac37b140-8e74-4955-ac3f-55a8c253211f",
-        description: "Buy something"
+        idMemory: crypto.randomUUID(),
+        idUser: "fe103b13-e357-4c6c-9aaf-dbe671f18887",
+        description: "Buy something",
+        done: false,
+        date: new Date("2023-12-01T10:00:00")
     };
-    const createMemory = new CreateMemory();
-    const output = await createMemory.execute(input);
-    expect(output.idMemory).toBe("ac37b140-8e74-4955-ac3f-55a8c253211f");
+    await createMemory.execute(input);
+    expect(memoryRepositorySpy.calledOnce).toBe(true);
+    memoryRepositorySpy.restore();
 });
 
 test("Should not create a new memory with an empty description", async function() {
     const input = {
-        idMemory: "ac37b140-8e74-4955-ac3f-55a8c253211f",
-        description: ""
+        idMemory: crypto.randomUUID(),
+        idUser: "fe103b13-e357-4c6c-9aaf-dbe671f18887",
+        description: "",
+        done: false,
+        date: new Date("2023-12-01T10:00:00")
     };
-    const createMemory = new CreateMemory();
     expect(() => createMemory.execute(input)).rejects.toThrow(new Error("Invalid description"));
 });
