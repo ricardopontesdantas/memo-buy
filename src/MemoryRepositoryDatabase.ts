10b0854b48d5c71776147f3feafcf8b4efc0d28f
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-06-16 08:16:59 -0300

    feat(memory-repository-database): implement method list

--- src/MemoryRepositoryDatabase.ts
+++ src/MemoryRepositoryDatabase.ts
@@ -1,16 +1,25 @@
 import pgp from "pg-promise";
-import { MemoryRepository } from "./MemoryRespository";
+import { MemoryRepository } from "./MemoryRepository";
 
-export default class MemoryRepositoryInMemory implements MemoryRepository {
+export default class MemoryRepositoryDatabase implements MemoryRepository {
     async save(memory: any): Promise<void> {
-        const connection = pgp()("");
-        await connection.query("insert into memory (idMemory, description) values ($1, $2)", [memory.idMemory, memory.description]);
+        const connection = pgp()("postgres://docker:ignite@localhost:5432/local");
+        await connection.query("insert into memobuy.memory (id_memory, id_user, description, done, created_at) values ($1, $2, $3, $4, $5)", [memory.idMemory, memory.idUser, memory.description, memory.done, memory.createdAt]);
         connection.$pool.end();
     }
 
     async updateDone(idMemory: string, done: boolean): Promise<any> {
-        const connection = pgp()("");
-        await connection.query("update memory set done = $1 where id_memory = $2", [done, idMemory]);
+        const connection = pgp()("postgres://docker:ignite@localhost:5432/local");
+        await connection.query("update memobuy.memory set done = $1 where id_memory = $2", [done, idMemory]);
+        const [updatedMemory] = await connection.query("select * from memobuy.memory where id_memory = $1", [idMemory]);
         connection.$pool.end();
+        return { idMemory: updatedMemory.id_memory, done: updatedMemory.done }
+    }
+
+    async list(idUser: string): Promise<any[]> {
+        const connection = pgp()("postgres://docker:ignite@localhost:5432/local");
+        const memoriesData = await connection.query("select * from memobuy.memory where id_user = $1", [idUser]);
+        connection.$pool.end();
+        return memoriesData;
     }
 }
