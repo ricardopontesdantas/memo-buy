fe32dc3b587700ef80b3b0e98b8907300b15cbff
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-06-16 08:18:25 -0300

    feat(api_rest): implement route for update done memory

--- src/api_rest.ts
+++ src/api_rest.ts
@@ -1,14 +1,14 @@
 import express, { Request, Response } from "express";
 import CreateMemory from "./CreateMemory";
+import MemoryRepositoryDatabase from "./MemoryRepositoryDatabase";
+import UpdateDoneMemory from "./UpdateDoneMemory";
 const app = express();
 app.use(express.json())
-const memories: Memory[] = [];
 
 app.post("/memories", async function(request: Request, response: Response) {
     const createMemory = new CreateMemory();
     try {
         const output = await createMemory.execute(request.body);
-        memories.push(output);
         return response.status(201).json(output);
     } catch (e: any) {
         return response.status(422).json({
@@ -18,15 +18,18 @@ app.post("/memories", async function(request: Request, response: Response) {
 });
 
 app.patch("/memories/:idMemory/done", async function(request: Request, response: Response) {
-    const memory = memories.find(memory => memory.idMemory === request.params.idMemory);
-    if (memory) memory.done = request.body.done;
-    return response.status(200).json(memory);
+    const updateDoneMemory = new UpdateDoneMemory();
+    try {
+        const updatedMemory = await updateDoneMemory.execute(request.params.idMemory, request.body.done);
+        return response.status(200).json({
+            idMemory: updatedMemory.idMemory,
+            done: updatedMemory.done
+        });
+    } catch (e: any) {
+        return response.status(422).json({
+            message: e.message
+        });
+    }
 });
-
-type Memory = {
-    idMemory: string,
-    description: string,
-    done: boolean
-}
 
 app.listen(3000);
