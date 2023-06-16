8c8ac6358e4e71c3abc4d3901c37dd903f41f8e8
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-06-16 13:20:57 -0300

    feat(api-rest): create route to list undone memories

--- src/api_rest.ts
+++ src/api_rest.ts
@@ -1,15 +1,15 @@
 import express, { Request, Response } from "express";
 import CreateMemory from "./CreateMemory";
-import MemoryRepositoryDatabase from "./MemoryRepositoryDatabase";
 import UpdateDoneMemory from "./UpdateDoneMemory";
+import GetUndoneMemories from "./GetUndoneMemories";
 const app = express();
 app.use(express.json())
 
 app.post("/memories", async function(request: Request, response: Response) {
     const createMemory = new CreateMemory();
     try {
-        const output = await createMemory.execute(request.body);
-        return response.status(201).json(output);
+        await createMemory.execute(request.body);
+        return response.status(201).end();
     } catch (e: any) {
         return response.status(422).json({
             message: e.message
@@ -20,15 +20,27 @@ app.post("/memories", async function(request: Request, response: Response) {
 app.patch("/memories/:idMemory/done", async function(request: Request, response: Response) {
     const updateDoneMemory = new UpdateDoneMemory();
     try {
-        const updatedMemory = await updateDoneMemory.execute(request.params.idMemory, request.body.done);
+        const output = await updateDoneMemory.execute(request.params.idMemory, request.body.done);
         return response.status(200).json({
-            idMemory: updatedMemory.idMemory,
-            done: updatedMemory.done
+            idMemory: output.idMemory,
+            done: output.done
         });
     } catch (e: any) {
         return response.status(422).json({
             message: e.message
         });
+    }
+});
+
+app.get("/memories/user/:idUser", async function(request: Request, response: Response) {
+    const getUndoneMemories = new GetUndoneMemories();
+    try {
+        const output = await getUndoneMemories.execute(request.params.idUser, new Date());
+        return response.status(200).json(output);
+    } catch (e: any) {
+        return response.status(422).json({
+            message: e.message
+        })
     }
 });
 
