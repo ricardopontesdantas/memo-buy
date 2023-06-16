905e40605ec29b92143a6b9013f8720a37fbdf16
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-06-16 13:23:23 -0300

    refactor(get-undone-memories): return without create a constant

--- src/GetUndoneMemories.ts
+++ src/GetUndoneMemories.ts
@@ -5,7 +5,6 @@ export default class GetUndoneMemories {
     constructor(readonly memoryRepository: MemoryRepository = new MemoryRepositoryDatabase()) {}
 
     async execute(idUser: string, today: Date): Promise<any> {
-        const output = await this.memoryRepository.listUndone(idUser, today);
-        return output;
+        return await this.memoryRepository.listUndone(idUser, today);
     }
 }
