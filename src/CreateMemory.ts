2c62eb625b5d8f7f731046dc56970b85f8c9a77c
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-06-18 10:46:10 -0300

    feat(create-memory): create an object memory with input data

--- src/CreateMemory.ts
+++ src/CreateMemory.ts
@@ -1,18 +1,18 @@
 import MemoryRepositoryDatabase from "./MemoryRepositoryDatabase";
 import { MemoryRepository } from "./MemoryRepository";
+import Memory from "./Memory";
 
 export default class CreateMemory {
     constructor(readonly memoryRepository: MemoryRepository = new MemoryRepositoryDatabase()) {}
 
     async execute(input: Input): Promise<void> {
-        if (input.description === "") throw new Error("Invalid description");
-        const memory = {
-            idMemory: input.idMemory,
-            idUser: input.idUser,
-            description: input.description,
-            done: input.done,
-            createdAt: input.date
-        };
+        const memory = new Memory(
+            input.idMemory,
+            input.idUser,
+            input.description,
+            input.done,
+            input.date
+        );
         await this.memoryRepository.save(memory);
     }
 }
