7d5533461db22a07d7245bd819b9858fdaef4437
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-06-18 10:42:25 -0300

    feat(memory): validate if description is empty

--- src/Memory.ts
+++ src/Memory.ts
@@ -1,3 +1,5 @@
 export default class Memory {
-    constructor(readonly idMemory: string, readonly idUser: string, readonly description: string, readonly done: boolean, readonly createdAt: Date) {}
+    constructor(readonly idMemory: string, readonly idUser: string, readonly description: string, readonly done: boolean, readonly createdAt: Date, readonly updatedAt: Date | null = null) {
+        if (this.description === "") throw new Error("Invalid description");
+    }
 }
