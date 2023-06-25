976711baea0d102fe6e899db486bedfc6cde3e04
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-06-25 10:22:27 -0300

    feat(database-repository-factory): receive connection from constructor

--- src/DatabaseRepositoryFactory.ts
+++ src/DatabaseRepositoryFactory.ts
@@ -1,9 +1,12 @@
+import DatabaseConnection from "./DatabaseConnection";
 import { MemoryRepository } from "./MemoryRepository";
 import MemoryRepositoryDatabase from "./MemoryRepositoryDatabase";
 import RepositoryFactory from "./RepositoryFactory";
 
 export default class DatabaseRepositoryFactory implements RepositoryFactory {
+    constructor(readonly connection: DatabaseConnection) {}
+    
     createMemoryRepository(): MemoryRepository {
-        return new MemoryRepositoryDatabase();
+        return new MemoryRepositoryDatabase(this.connection);
     }
 }
