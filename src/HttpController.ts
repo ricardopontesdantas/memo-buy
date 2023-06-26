bed1c74f3d08361576e03ff289bbe4169ddb1a19
Ricardo Pontes Dantas <ricardopontesdantas@gmail.com>
2023-06-26 08:44:23 -0300

    feat(http-controller): craete a controller receiving http server in constructor

---
+++ src/HttpController.ts
@@ -0,0 +1,26 @@
+import CreateMemory from "./CreateMemory";
+import GetUndoneMemories from "./GetUndoneMemories";
+import HttpServer from "./HttpServer";
+import UpdateDoneMemory from "./UpdateDoneMemory";
+
+export default class HttpController {
+    constructor(readonly httpServer: HttpServer, readonly createMemory: CreateMemory, readonly updateDoneMemory: UpdateDoneMemory, getUndoneMemories: GetUndoneMemories) {
+        this.httpServer.on("post", "/memories", async function(params: any, body: any) {
+            const output = await createMemory.execute(body);
+            return output;
+        });
+
+        this.httpServer.on("patch", "/memories/:idMemory/done", async function(params: any, body: any) {
+            const output = await updateDoneMemory.execute(params.idMemory, body.done);
+            return {
+                idMemory: output.idMemory,
+                done: output.done
+            };
+        });
+
+        this.httpServer.on("get", "/memories/user/:idUser", async function(params: any, body: any) {
+            const output = await getUndoneMemories.execute(params.idUser, new Date());
+            return output;
+        });
+    }
+}
